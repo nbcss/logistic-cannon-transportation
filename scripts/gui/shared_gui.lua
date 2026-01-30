dd92606837ae31df286b7757051b44c07f665f72
FiveYellowMice <hkz85825915@gmail.com>
2026-01-30 19:48:31 +1100

    Add signal tooltip

--- scripts/gui/shared_gui.lua
+++ scripts/gui/shared_gui.lua
@@ -1,4 +1,5 @@
 local constants = require("constants")
+local format = require("scripts.format")
 
 local circuit_control_header = {}
 
@@ -28,11 +29,19 @@ function circuit_control_header.create(parent, name)
         type = "label",
         name = "red_network",
         visible = false,
+        raise_hover_events = true,
+        tags = {
+            [constants.gui_tag_tracked_hover_state] = false,
+        },
     }
     element.flow.add {
         type = "label",
         name = "green_network",
         visible = false,
+        raise_hover_events = true,
+        tags = {
+            [constants.gui_tag_tracked_hover_state] = false,
+        },
     }
     element.flow.add {
         type = "empty-widget",
@@ -52,12 +61,49 @@ function circuit_control_header.refresh(element, station)
         and { "logistic-cannon-transportation.circuit-control-connected" }
         or { "logistic-cannon-transportation.circuit-control-unconnected" },
     }
-    element.flow.red_network.visible = red_network ~= nil
-    element.flow.red_network.caption = red_network and
-        string.format("[color=red]%s[/color] [img=info]", red_network.network_id) or ""
-    element.flow.green_network.visible = green_network ~= nil
-    element.flow.green_network.caption = green_network and
-        string.format("[color=green]%s[/color] [img=info]", green_network.network_id) or ""
+    ---@param color string
+    ---@param network LuaCircuitNetwork?
+    ---@param label LuaGuiElement
+    function network_label(color, network, label)
+        if network then
+            label.visible = true
+            label.caption = string.format("[color=%s]%s[/color] [img=info]", color, network.network_id)
+            if label.tags[constants.gui_tag_tracked_hover_state] then
+                local tooltip = {}
+                local signals = network.signals
+                for i, signal in ipairs(signals or {}) do
+                    local sprite_type
+                    if signal.signal.type == nil then
+                        sprite_type = "item"
+                    elseif signal.signal.type == "virtual" then
+                        sprite_type = "virtual-signal"
+                    else
+                        sprite_type = signal.signal.type
+                    end
+                    local quality = signal.signal.quality
+                    local sep = i == #signals and "" or (i % 4 == 0 and "\n" or "\xe2\x80\x83"--[[em space]])
+                    if quality == nil then
+                        table.insert(tooltip, string.format("[img=%s/%s]%s%s",
+                            sprite_type, signal.signal.name, format.number(signal.count), sep))
+                    else
+                        table.insert(tooltip, string.format("[img=%s/%s][img=quality/%s]%s%s",
+                            sprite_type, signal.signal.name, quality, format.number(signal.count), sep))
+                    end
+                end
+                label.tooltip = { "",
+                    "[color=#fae8be][font=default-semibold]", { "description.signals" }, "[/font][/color]\n",
+                    table.concat(tooltip) }
+            else
+                label.tooltip = nil
+            end
+        else
+            label.visible = false
+            label.caption = nil
+            label.tooltip = nil
+        end
+    end
+    network_label("red", red_network, element.flow.red_network)
+    network_label("green", green_network, element.flow.green_network)
 end
 
 return {
